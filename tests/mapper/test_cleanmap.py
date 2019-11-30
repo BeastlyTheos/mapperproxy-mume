218ca48ea58955113334aff57727fb555567a99a
Ted <TheodoreCooke@trentu.ca>
2019-11-30 17:25:31 -0500

    t. Revert "Only auto-clean erroneous secret exits if automatic room updating is enabled."
    This reverts commit f3ac319a1ebab67d8a7c1d5af4bf4ec9c7746921.

--- tests/mapper/test_cleanmap.py
+++ tests/mapper/test_cleanmap.py
@@ -52,7 +52,6 @@ class TestExitsCleaner(unittest.TestCase):
 	def setUp(self):
 		self.mapper = Mock(spec=Mapper)
 		self.mapper.isSynced = True
-		self.mapper.autoUpdateRooms = True
 		self.exitsCleaner = ExitsCleaner(self.mapper)
 
 	def createRoom(self, *exits):
